8e1dc0db077ea552039867b07408e6a85e55fa85
Carl Boettiger <cboettig@gmail.com>
2014-05-29 13:54:22 -0700

    documentation updated

--- manuscripts/components/install.R
+++ manuscripts/components/install.R
@@ -2,15 +2,15 @@
 library("methods")
 
 if(!require("template")){
-#  install.packages("devtools")
-#  library("devtools")
-#  install_github("reshape")
-  install.packages("..", repos =NULL, dependencies = c("Depends", "Imports", "Suggests"))
+  install.packages("devtools")
+  library("devtools")
+  install_github("reshape")
+  install_github("cboettig/template", dependencies = c("Depends", "Imports", "Suggests"))
 }
 
 # Use the packrat.lock file to verify dependencies. 
 library("packrat")
-packrat::packify(".")
+packrat::packify()
 source(".Rprofile")
 readRenviron(".Renviron")
 packrat::restore()
