3c4e2b252550bb30e5f8e11753c9b7e296502363
Carl Boettiger <cboettig@gmail.com>
2014-05-29 14:50:45 -0700

    testing packrat

--- manuscripts/components/install.R
+++ manuscripts/components/install.R
@@ -1,9 +1,13 @@
 #!/usr/bin/Rscript
 library("methods")
 # Install packrat if not available
+options(repos=c(CRAN="http://cran.rstudio.com"))
 if(!require("devtools")) install.packages("devtools")
 if(!require("packrat")) devtools::install_github("rstudio/packrat")
 
+packrat::packify()
+source(".Rprofile"); readRenviron(".Renviron")
+packrat::restore()
 
 
 
