6432b9783725ecb2731ce4082ac9647356f6b4a2
Carl Boettiger <cboettig@gmail.com>
2017-11-05 21:09:33 -0800

    draft for `template` function to deploy template. Still needs opt-in github api functionality

---
+++ R/template.R
@@ -0,0 +1,29 @@
+library(git2r)
+
+#' template
+#'
+#' Clone a template, change remote origin, push
+#' @param template url to the template repository
+#' @param dest destination remote url
+#' @param public should the new GitHub repository be public or private?
+#'
+#' @param ... additional arguments to git2r functions
+#'
+#' @details url can be any git address we can use to clone (e.g. https://, git@@github.com:)
+#' @importFrom git2r clone remote_add remote_remove push
+#' @importFrom gh gh
+#' @examples
+#' \dontrun{
+#' template("https://github.com/cboettig/template",
+#'          "https://gtihub.com/boettiger-lab/mypkg")
+#' }
+template <- function(template, dest, public = FALSE, ...){
+  repo <- git2r::clone(template)
+  git2r::remote_remove(repo, "origin")
+  git2r::remote_add(repo, "origin", dest)
+
+  ## Create repo via the GitHub API
+
+  #git2r::push(repo)
+}
+
